82c698ba4d99c3ae82fbffaf77786c60004e383e
GuiJg <j.gu1lh3rm325@gmail.com>
2023-07-31 17:05:09 -0300

    feat: style banner

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+//navbar mobile
+const menuButton = document.querySelector('.toggle');
+const headerMenu = document.querySelector('.header-menu');
+  
+  menuButton.addEventListener('click', function() {
+      headerMenu.classList.toggle('active');
+  });
+
+  window.addEventListener('scroll', function(){
+
+    const button = document.querySelector('.btn-content');
+    let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+    if (scrollPosition > 100) {
+      button.style.opacity = '1';
+    } else {
+      button.style.opacity = '0';
+    }
+  });
