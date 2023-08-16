0558a29cfb321b01ea4c0a5120ecfadcca8555de
GuiJg <j.gu1lh3rm325@gmail.com>
2023-08-16 18:29:04 -0300

    feat: social icons fixed add

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,29 @@ const headerMenu = document.querySelector('.header-menu');
       headerMenu.classList.toggle('active');
   });
 
-  window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function(){
 
-    const button = document.querySelector('.btn-content');
-    let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  const button = document.querySelector('.btn-content');
+  let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   
-    if (scrollPosition > 100) {
-      button.style.opacity = '1';
-    } else {
-      button.style.opacity = '0';
-    }
-  });
+  if (scrollPosition > 100) {
+    button.style.opacity = '1';
+  } else {
+    button.style.opacity = '0';
+  }
+});
+
+window.addEventListener('scroll', function(){
+
+  const button = document.querySelector('.social-fixed');
+  let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollPosition > 100) {
+    button.style.opacity = '1';
+  } else {
+    button.style.opacity = '0';
+  }
+});
 
 window.addEventListener("scroll", function(){
   let header = document.querySelector(".header");
